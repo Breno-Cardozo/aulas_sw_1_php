3835df48a3c1ec93485ff5b20b5db944b7face04
Breno Cardozo Fagundes <79853219+Breno-Cardozo@users.noreply.github.com>
2022-04-28 08:05:45 -0300

    Add files via upload

---
+++ aula_28_04_22/revisao/exemple.php
@@ -0,0 +1,16 @@
+<?php
+    $num=10;
+
+    if ($num > 10){
+        echo "Número é maior que 10";
+    }
+    else{
+        if($num<10){
+            echo "Número é menor que 10";
+        }
+        else {
+            echo "Número é igual a 10";
+        }
+    }
+    //echo "Não faz nada!";
+?>
