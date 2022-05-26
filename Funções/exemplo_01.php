5ee04dd142c8a8344b9afe32fdb9741d40b718a0
Breno Cardozo Fagundes <79853219+Breno-Cardozo@users.noreply.github.com>
2022-05-26 09:08:22 -0300

    Add files via upload

---
+++ Funções/exemplo_01.php
@@ -0,0 +1,15 @@
+<?php
+//Funções
+/*
+1-Função sem retorno e sem parametro;
+*/
+
+function teste (){
+    echo "Breno Cardozo Fagundes";
+}
+
+teste();
+
+
+
+?>
---
+++ Funções/exemplo_03.php
@@ -0,0 +1,16 @@
+<?php
+//Função com retorno e sem parametro
+
+function teste3(){
+    $b = 'Breno';
+    return $b;
+}
+$resultado = teste3();
+echo $resultado;
+
+echo "<hr>"
+
+
+
+
+?>
